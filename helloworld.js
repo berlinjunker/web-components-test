32522404385f73f5eb59cce06838ec50430f7089
Peter Werner <werner@mobile-software.ag>
2023-09-27 09:39:41 +0200

    applying slots

--- helloworld.js
+++ helloworld.js
@@ -1,36 +1,34 @@
+// web component
 class HelloWorld extends HTMLElement {
-
+  
+  // component attributes
   static get observedAttributes() {
     return ['name'];
   }
-
-  connectedCallback() {
-    const shadow = this.attachShadow({ mode: 'closed' });
-
-    shadow.innerHTML = `
-    <style>
-      p {
-        text-align: center;
-        font-weight: normal;
-        padding: 1em;
-        margin: 0 0 2em 0;
-        background-color: #eee;
-        border: 1px solid #666;
-      }
-    </style>
-    
-    <p>Hello ${ this.name }!</p>
-    `;
-
-    // this.textContent = `Hello ${ this.name }!`;
-  }
-
+  
+  // attribute change
   attributeChangedCallback(property, oldValue, newValue) {
     if (oldValue === newValue) return;
     this[ property ] = newValue;
   }
-
+  
+  // connect component
+  connectedCallback() {
+    const 
+      shadow = this.attachShadow({ mode: 'closed' }),
+      hwMsg = `Hello ${ this.name }`;
+    
+    // append shadow DOM
+    shadow.append( 
+      document.getElementById('hello-world').content.cloneNode(true)
+    );
+   
+    // find all slots with a hw-text class
+    Array.from( shadow.querySelectorAll('slot.hw-text') )
+      // update first assignedNode in slot
+     .forEach( n => n.assignedNodes()[0].textContent = hwMsg );
+  }
 }
 
 // register component
-customElements.define( 'hello-world', HelloWorld );
+customElements.define('hello-world', HelloWorld);
